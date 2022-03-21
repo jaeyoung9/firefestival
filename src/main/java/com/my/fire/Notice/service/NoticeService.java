9e2632b18d0f2b0b5bf526b6b6fb7c2090bc835e
jaeyoung9 <workerst@naver.com>
2022-03-21 14:50:58 +0900

    s

---
+++ src/main/java/com/my/fire/Notice/service/NoticeService.java
@@ -0,0 +1,25 @@
+package com.my.fire.Notice.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+//민재영 - notice
+public interface NoticeService {
+
+	// 공지페이지
+	List<Map<String, Object>> notice(Map<String, Object> map) ;
+
+	// 공지상세페이지
+	List<Map<String, Object>> noticeDetail(Map<String, Object> map);
+	
+	// 공지작성dd
+	void noticeGo(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
+	// 공지수정dd
+	void noticeUp(Map<String, Object> map) throws Exception;
+
+	// 공지삭제
+	void noticeDe(Map<String, Object> map) throws Exception;
+}
