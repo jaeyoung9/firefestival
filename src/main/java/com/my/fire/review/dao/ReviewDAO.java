3387266cf9d15afcb2ff935c0312a70e1dce0591
KHyungTae <gudxoehdzz@nate.com>
2022-03-22 12:37:30 +0900

    gudxoehd

--- src/main/java/com/my/fire/review/dao/ReviewDAO.java
+++ src/main/java/com/my/fire/review/dao/ReviewDAO.java
@@ -1,5 +1,41 @@
 package com.my.fire.review.dao;
 
-public class ReviewDAO {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
+import com.my.fire.common.dao.AbstractDAO;
+
+@Repository("reviewDAO")
+public class ReviewDAO extends AbstractDAO{
+
+	// 리뷰 페이지
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> reviewPage(Map<String, Object> map) {
+		return (List<Map<String, Object>>) selectPagingList("review.reviewPage", map);
+	}
+	
+	// 리뷰 작성
+	@SuppressWarnings("unchecked")
+	public void reviewUserWrite(Map<String, Object> map) {
+		insert("review.reviewUserWrite", map);
+	}
+	
+	// 리뷰 상세보기 페이지 이동 	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> reviewDetail(Map<String, Object> map){
+		return (List<Map<String, Object>>) selectList("review.reviewDetail",map);
+	}
+	// 리뷰 삭제
+	public void reviewDelete(Map<String, Object> map) throws Exception {
+		delete("review.reviewDelete", map);	
+	}
+	
+	//리뷰 수정
+	public void reviewUpdate(Map<String, Object> map) throws Exception {
+		update("review.reviewUpdate", map);
+	}
+	
+	
 }
